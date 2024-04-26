0e979e872ae2547b6a4fc9f12306d2a4498a3185
CeysInSpace <159135471+CeysInSpace@users.noreply.github.com>
2024-04-26 15:09:31 +0200

    Site deel

---
+++ js/mapScript.js
@@ -0,0 +1,12 @@
+let map = L.map("apMap").setView([51.23009, 4.41616], 17);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+let marker = L.marker([51.23009, 4.41616]).addTo(map);
+let bounds = [
+  [51.23041, 4.4155],
+  [51.22991, 4.41675],
+];
+L.rectangle(bounds, { color: "#e60005", weight: 1 }).addTo(map);
